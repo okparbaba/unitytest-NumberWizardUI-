e353d938ff620196e0e2b89c011299a872dbca13
yannaing-lynn <rghtbossy@gmail.com>
2020-04-19 13:22:34 +0630

    guess text

--- Assets/Scripts/NumberWizard.cs
+++ Assets/Scripts/NumberWizard.cs
@@ -1,11 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class NumberWizard : MonoBehaviour {
 
     [SerializeField] int max;
     [SerializeField] int min;
+    [SerializeField] TextMeshProUGUI guessText;
     int guess;
 
     // Use this for initialization
@@ -16,6 +18,8 @@ public class NumberWizard : MonoBehaviour {
 	
     void StartGame()
     {
+        guess = (max + min) / 2;
+        guessText.text = guess.ToString();
         max = max + 1;
     }
     public void OnPressHigher()
@@ -32,5 +36,6 @@ public class NumberWizard : MonoBehaviour {
     void NextGuess()
     {
         guess = (max + min) / 2;
+        guessText.text = guess.ToString();
     }
 }
